fe2fd1c3a71d6ca00503c61b60eaafbebe12ae89
Gaga <lalisa.tamene@aastustudent.edu.et>
2025-10-03 10:21:18 +0300

    the error on projects card is solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,38 @@
 /*=============== HOME SPLIT TEXT ===============*/
 
-
 /*=============== SWIPER PROJECTS ===============*/
-const swiperProjects = new Swiper('.project-info', {
+const swiperProjects = new Swiper(".project-info", {
   loop: true,
+  slidesPerView: 'auto',
   spaceBetween: 24,
   grabCursor: true,
   speed: 600,
 
-
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
   },
 
-//   autoplay: {
-//     delay: 3000,
-//     disableOnInteraction: false,
-//   }
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 });
 
 /*=============== WORK TABS ===============*/
 
-
 /*=============== SERVICES ACCORDION ===============*/
-
 
 /*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
 
-
 /*=============== COPY EMAIL IN CONTACT ===============*/
 
-
-/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
-
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
-
 /*=============== CUSTOM CURSOR ===============*/
 
-
 /* Hide custom cursor on links */
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
